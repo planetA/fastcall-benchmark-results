e941a4843ea0385f42f2558a7f485a681dddce91
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2023-11-10 22:20:22 +0100

    Update analysis for the thesis

--- thesis.R
+++ thesis.R
@@ -1,7 +1,9 @@
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
+library(ggrepel)
 library(tikzDevice)
+library(RColorBrewer)
 options(width = 260)
 
 CPUS <- c(
@@ -211,6 +213,8 @@ df.oci <- df.noop %>%
         cpu == cur.cpu,
     )
 
+my_palette <- brewer.pal(name = "Dark2", n = 3)[2:3]
+
 # Make a barplot
 p <- df.oci %>%
     mutate(
@@ -225,8 +229,10 @@ p <- df.oci %>%
     ) %>%
     ggplot(aes(x = mechanism, y = real_time, fill = mitigation)) +
     geom_bar(stat = "identity", position = "dodge") +
-    scale_fill_brewer(
-        type = "qual", palette = "Dark2", name = "Mitigations", direction = -1
+    scale_fill_manual(
+        values = my_palette,
+        name = "Mitigations"
+        # type = "qual", palette = "Dark2", direction = 1
     ) +
     geom_text(aes(label = real_time),
         position = position_dodge(width = 0.9), vjust = -0.5, size = 2.5
@@ -261,7 +267,7 @@ plot(p)
 # Save the plot as tikz
 tikz(
     file.path("plots", cur.cpu, "latency", "latency.tex"),
-    width = 2.3, height = 2.5
+    width = 2.3, height = 2.2
 )
 plot(p)
 dev.off()
@@ -361,7 +367,7 @@ df.spec <- cols(
     `vfork-fastcall` = col_integer()
 )
 
-df <- data.frame()
+df.all <- data.frame()
 for (cpu in CPUS) {
     # Traverse the CPU directory
     cpu_dir <- file.path("results", cpu)
@@ -381,7 +387,7 @@ for (cpu in CPUS) {
             }
             print(paste0("Reading ", mechanism_path))
 
-            df <- read_csv(mechanism_path, col_type = df.spec) %>%
+            df.all <- read_csv(mechanism_path, col_type = df.spec) %>%
                 pivot_longer(
                     cols = everything(),
                     names_to = "measurement",
@@ -393,18 +399,21 @@ for (cpu in CPUS) {
                     mechanism = mechanism,
                     path = mechanism_path,
                 ) %>%
-                bind_rows(df)
+                bind_rows(df.all)
         }
     }
 }
 
-df <- df %>%
+df <- df.all %>%
     group_by(
         cpu, mitigation, mechanism, measurement
     ) %>%
+    filter(
+        time < quantile(time, 0.99),
+    ) %>%
     summarise(
         time.sd = sd(time),
-        time = mean(time),
+        time = median(time),
     )
 
 df.reg <- df %>%
@@ -429,14 +438,15 @@ p <- df.reg %>%
     ) %>%
     mutate(
         mechanism = fct_recode(mechanism,
-            "Unprivileged" = "ycall",
-            "Privileged" = "fastcall",
+            "Unpriv." = "ycall",
+            "Priv." = "fastcall",
         ),
+        measurement = fct_reorder(measurement, time),
     ) %>%
     left_join(df.cpu.freq, by = "cpu") %>%
     arrange(time) %>%
     ggplot(aes(
-        x = fct_reorder(measurement, time),
+        x = fct_reorder(mechanism, time),
         y = time / 1000, fill = mechanism
     )) +
     geom_bar(stat = "identity", position = "dodge") +
@@ -453,15 +463,285 @@ p <- df.reg %>%
     theme_pubr(base_size = 8) %+replace%
     theme(
         strip.background = element_blank(),
-        strip.placement = "outside", legend.position = "none"
+        strip.placement = "outside",
+        legend.position = "none",
+        axis.text = element_text(size = 7)
     ) +
-    facet_wrap(~mechanism, scales = "free_x")
+    facet_wrap(~measurement, scales = "free_x")
 plot(p)
 
 # Save the plot as tikz
 tikz(
-    file.path("plots", cur.cpu, "misc", "reg-dereg.tex"),
+    file.path("plots", cur.cpu, "reg-dereg.tex"),
     width = 2.3, height = 2.5
 )
 plot(p)
 dev.off()
+
+df.fork <-
+    df %>%
+    ungroup() %>%
+    filter(
+        str_count(measurement, "fork") > 0,
+        str_count(measurement, "vfork") == 0,
+        mitigation == "mitigations=auto",
+        # mechanism == "fastcall",
+        cpu == cur.cpu
+    ) %>%
+    separate_wider_delim(
+        measurement,
+        names = c("fork", "base"), delim = "-"
+    ) %>%
+    select(-mitigation, -time.sd) %>%
+    mutate(base = if_else(base == "ycall", "fastcall", base)) %>%
+    group_by(cpu, mechanism, fork) %>%
+    pivot_wider(
+        names_from = base,
+        values_from = time
+    ) %>%
+    left_join(df.cpu.freq, by = "cpu") %>%
+    mutate(
+        fastcall = fastcall - simple,
+    ) %>%
+    pivot_longer(
+        cols = c("fastcall", "simple"),
+        names_to = "run",
+        values_to = "time"
+    )
+# mutate(
+#     overhead = time_fastcall - time_simple,
+#     overhead.cycles = overhead * freq / 1000,
+# ) %>%
+
+df.label <- data.frame(
+    x = c(1.45, 2),
+    y = c(43, 42),
+    xend = c("Unprivileged", "Unprivileged"),
+    yend = c(45, 45)
+)
+
+p <- df.fork %>%
+    ungroup() %>%
+    mutate(
+        mechanism = fct_recode(mechanism,
+            "Unprivileged" = "ycall",
+            "Privileged" = "fastcall",
+        ),
+    ) %>%
+    ggplot(aes(
+        x = mechanism,
+        y = time / 1000, fill = run
+    )) +
+    geom_col() +
+    geom_segment(
+        data = df.label,
+        aes(x = x, y = y, xend = xend, yend = yend),
+        alpha = 0.5,
+        arrow = arrow(length = unit(0.10, "cm"), ends = "first", type = "closed"),
+        inherit.aes = FALSE
+    ) +
+    geom_text(
+        data = df.label %>% head(n = 1),
+        aes(x = xend, y = yend, label = "With fastcalls"),
+        position = position_dodge(width = 0.9), vjust = -0.5, size = 2.5,
+        inherit.aes = FALSE
+    ) +
+    scale_fill_brewer(
+        type = "qual",
+        palette = "Dark2", name = "Mitigations", direction = -1
+    ) +
+    expand_limits(y = 47) +
+    ylab("Time (us)") +
+    xlab("Fastcall") +
+    theme_pubr(base_size = 8) %+replace%
+    theme(
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        legend.position = "none",
+        axis.text = element_text(size = 7)
+    )
+plot(p)
+
+# Save the plot as tikz
+tikz(
+    file.path("plots", cur.cpu, "fork.tex"),
+    width = 1.8, height = 2.5
+)
+plot(p)
+dev.off()
+
+df.noop %>%
+    filter(
+        mitigation == "Default" | cpu == "Neoverse-N1"
+    ) %>%
+    select(
+        -bytes_per_second,
+        -items_per_second,
+        -error_occurred,
+        -error_message,
+        -Size,
+        -Benchmark,
+        -time_unit,
+        -path,
+        -mitigation,
+        -label,
+        -cpu_time,
+        -iterations
+    ) %>% 
+    left_join(df.cpu.freq, by = "cpu") %>%
+    mutate(
+    #    real_time = real_time * freq / 1000,
+    ) %>%
+    pivot_wider(
+        names_from = mechanism,
+        values_from = real_time
+    ) %>%
+    select(
+        short_cpu, freq, vDSO, `FC (U)`, `FC (P)`, Syscall, ioctl
+    ) %>%
+    mutate(
+        i = c(5, 4, 3, 1, 6, 2)
+    ) %>%
+    #filter(short_cpu != "Neoverse-N1") %>%
+    arrange(i) %>%
+    mutate(ioctl / Syscall) 
+
+UNITS <- c("misc", "cycles")
+KERNELS <- c("fastcall", "fccmp", "ycall")
+MITIGATIONS <- c("mitigations=off")
+df <- data.frame()
+for (cpu in CPUS) {
+    # Traverse the CPU directory
+    cpu_dir <- file.path("results", cpu)
+    for (mitigation in MITIGATIONS) {
+        # Traverse the mitigation directory
+        mitigation_dir <- file.path(cpu_dir, mitigation)
+        # Traverse the kernels
+        for (kernel in KERNELS) {
+            for (unit in UNITS) {
+                mechanism_path <- file.path(
+                    mitigation_dir, paste0(unit, "-", kernel, ".csv")
+                )
+
+                # Skip if file does not exist
+                if (!file.exists(mechanism_path)) {
+                    print(paste0("Skipping ", mechanism_path))
+                    next
+                }
+
+                df <- read_csv(mechanism_path, col_type = df.spec) %>%
+                    mutate(
+                        cpu = cpu,
+                        mitigation = mitigation,
+                        kernel = kernel,
+                        path = mechanism_path,
+                        i = row_number(),
+                        unit = if_else(
+                            unit == "misc", "ns", "cycles"
+                        )
+                    ) %>%
+                    bind_rows(df)
+            }
+        }
+    }
+}
+
+#df.noop 
+df %>%
+    select(
+        -`ycall-registration-minimal`,
+        -`ycall-deregistration-minimal`,
+        -`fork-simple`,
+        -`fork-fastcall`,
+        -`fork-ycall`,
+        -`vfork-simple`,
+        -`vfork-fastcall`,
+        -`vfork-ycall`,
+        -`registration-minimal`,
+        -`registration-mappings`,
+        -`deregistration-minimal`,
+        -`deregistration-mappings`,
+    ) %>%
+    pivot_longer(
+        cols = c(
+            `noop`,
+            `ycall`,
+            `vdso`,
+            `syscall`,
+            `ioctl`,
+            `fastcall`
+        ),
+        names_to = "mechanism",
+        values_to = "time"
+    ) %>%
+    # mutate(
+    #     old_mechanism = mechanism,
+    #     mechanism = if_else(mechanism == "noop", kernel, mechanism),
+    # ) %>%
+    filter(
+        !is.na(time),
+        # i == 1
+    ) %>% 
+    group_by(
+        cpu, mitigation, kernel,
+        mechanism,
+        unit
+    ) %>%
+    summarise(
+        n = n(),
+        # time.sd = sd(time),
+        time = median(time),
+    ) %>%
+    ungroup() %>% filter(unit == "cycles") %>%
+    filter(
+        !(mechanism == "noop" & kernel == "fastcall"),
+        !(mechanism == "noop" & kernel == "ycall")
+    ) %>%
+    select(
+        -n,
+        -kernel
+    ) %>%
+    pivot_wider(
+        names_from = mechanism,
+        values_from = time
+    ) %>%
+    mutate(
+        fastcall_d = fastcall - noop,
+        ycall_d = ycall - noop,
+        ioctl_d = ioctl - noop,
+        syscall_d = syscall - noop,
+        vdso_d = vdso - noop
+    )
+
+    select(
+        -n,
+        -kernel
+    ) %>%
+    pivot_wider(
+        names_from = mechanism,
+        values_from = time
+    ) %>%
+    arrange(unit, cpu, kernel)
+
+read_csv("cycle_stats.csv") %>% 
+    filter(mitigation == "mitigations=off") %>%
+    group_by(CPU, mitigation, kernel, benchmark) %>%
+    select(adjusted, median) %>%
+    pivot_wider(
+        names_from = benchmark,
+        values_from = median
+    ) %>% print(n = Inf)
+    mutate(
+        i = row_number()
+    ) %>%
+    filter(
+        i %in% c(3897, 3898, 3899, 6216, 6217, 6218)
+    )
+    pivot_longer(
+        cols = everything(),
+        names_to = "measurement",
+        values_to = "time"
+    ) %>%
+    drop_na()
+    arrange(-time)
+
